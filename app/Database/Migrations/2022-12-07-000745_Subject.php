e04e35794e8b6d5a306da9d9594b527ebb648bb6
Waskito <wmeliantoo@gmail.com>
2022-12-07 10:12:41 +0700

    Commit API

---
+++ app/Database/Migrations/2022-12-07-000745_Subject.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Subject extends Migration
+{
+    public function up() {
+        $this->forge->addField([
+        'book_id' => [
+            'type' => 'INT',
+            'unsigned' => TRUE,
+        'auto_increment' => TRUE
+        ],
+        'title' => [
+        'type' => 'VARCHAR',
+        'constraint' => 100,
+        'null' => FALSE
+        ],
+        'year' => [
+        'type' => 'INT',
+        'null' => FALSE
+        ],
+        'author' => [
+        'type' => 'VARCHAR',
+        'constraint' => 100,
+        'null' => FALSE
+        ],
+        'publisher' => [
+        'type' => 'VARCHAR',
+        'constraint' => 100,
+        'null' => FALSE
+        ],
+        'status' => [
+        'type' => 'VARCHAR',
+        'constraint' => 100,
+        'null' => FALSE
+        ],
+        'price' => [
+        'type' => 'INT',
+        'null' => FALSE
+        ]
+        ]);
+        $this->forge->addKey('book_id', TRUE);
+        
+        $this->forge->createTable('book');
+        
+        }
+    public function down() {
+        $this->forge->dropTable('book');
+        
+        }
+}
+
